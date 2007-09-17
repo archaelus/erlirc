7e99ffcc755048e7c62373b1825e81d7eebc39e4
Geoff Cant <nem@lisp.geek.nz>
2007-09-17 20:23:46 +1200

    Adding an example irc server based on gen_irc_server.

---
+++ src/simple_irc_server.erl
@@ -0,0 +1,140 @@
+%%%-------------------------------------------------------------------
+%% @copyright Geoff Cant
+%% @author Geoff Cant <nem@erlang.geek.nz>
+%% @version {@vsn}, {@date} {@time}
+%% @doc Simple IRC server.
+%% @end
+%%%-------------------------------------------------------------------
+-module(simple_irc_server).
+
+-behaviour(gen_irc_server).
+
+-include_lib("logging.hrl").
+-include_lib("eunit.hrl").
+-include_lib("kernel/include/inet.hrl").
+
+%% API
+-export([start_link/0,
+         start_link/2,
+         shutdown/1]).
+
+%% gen_irc_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3,
+	 handle_nick/3]).
+
+-record(state, {local_nicks = []}).
+
+-define(SERVER, ?MODULE).
+-define(PORT, 16667).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% @spec start_link() -> {ok,Pid} | ignore | {error,Error}
+%% @doc Starts the server
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    start_link(atom_to_list(?MODULE), ?PORT).
+
+start_link(Name, Port) ->
+    {ok, Pid} = gen_irc_server:start_link(?MODULE, Name, []),
+    {ok, _Listener} = gen_irc_server:listen(Pid, Port),
+    {ok, Pid}.
+
+
+shutdown(Server) ->
+    gen_server:call(Server, shutdown).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore               |
+%%                     {stop, Reason}
+%% @doc Initialises the server's state
+%% @end
+%%--------------------------------------------------------------------
+init(_) ->
+    {ok, #state{}}.
+
+handle_nick(Nick, _Pass, S = #state{local_nicks=LN}) ->
+    case lists:member(Nick, LN) of
+	true ->
+	    {reply, {invalid, nicknameinuse, "Nickname in use."}, S};
+	false ->
+	    {reply, valid, S#state{local_nicks=[Nick|LN]}}
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @spec 
+%% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% @doc Call message handler callbacks
+%% @end
+%%--------------------------------------------------------------------
+handle_call(Call, _From, State) ->
+    ?WARN("Unexpected call ~p.", [Call]),
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @spec 
+%% handle_cast(Msg, State) -> {noreply, State} |
+%%                            {noreply, State, Timeout} |
+%%                            {stop, Reason, State}
+%% @doc Cast message handler callbacks
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(Msg, State) ->
+    ?WARN("Unexpected cast ~p", [Msg]),
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @spec 
+%% handle_info(Info, State) -> {noreply, State} |
+%%                             {noreply, State, Timeout} |
+%%                             {stop, Reason, State}
+%% @doc Non gen-server message handler callbacks
+%% @end
+%%--------------------------------------------------------------------
+handle_info(Info, State) ->
+    ?WARN("Unexpected info ~p", [Info]),
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @spec terminate(Reason, State) -> void()
+%% @doc This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @doc Convert process state when code is changed
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
