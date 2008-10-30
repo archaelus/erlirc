17e01fee0aecc5ceaea866940f559a691d03d061
Geoff Cant <nem@erlang.geek.nz>
2008-10-30 19:24:54 +0100

    Allowing bare pids in the From section of an irc message. Adding message examples (should be exhaustive if kept up to date).

--- src/gen_irc.erl
+++ src/gen_irc.erl
@@ -9,23 +9,37 @@
 
 %% API
 -export([msg/4
-         ,msg/5]).
+         ,msg/3]).
 
 %%====================================================================
 %% API
 %%====================================================================
 
-msg(Type, Ref, {Pid, Name}, Msg) ->
-    msg(Type, Ref, Pid, Name, Msg);
-msg(Type, Ref, Name, Msg) ->
-    msg(Type, Ref, self(), Name, Msg).
-
-msg(Type, Ref, Pid, Name, Msg)
-  when Type =:= server orelse Type =:= user orelse Type =:= channel,
-       is_reference(Ref),
-       is_pid(Pid) orelse is_atom(Pid) ->
-    {irc, Type, Ref, {Pid, Name}, Msg}.
+msg(Type, Ref, Msg) ->
+    msg2(Type, Ref, self(), Msg).
+                            
+msg(Type, Ref, {Pid, Name}, Msg)
+  when is_pid(Pid) orelse is_atom(Pid), is_list(Name) ->
+    msg2(Type, Ref, {Pid, Name}, Msg);
+msg(Type, Ref, Name, Msg) when is_list(Name)->
+    msg2(Type, Ref, {self(), Name}, Msg).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+msg2(Type, Ref, From, Msg)
+  when Type =:= server orelse Type =:= user orelse Type =:= channel,
+       is_reference(Ref) ->
+    {irc, Type, Ref, From, Msg}.
+
+%%====================================================================
+%% Message Examples
+%%====================================================================
+
+%% From = {username, Pid} | {channelname, Pid} | Pid
+
+%% {irc, user, Ref, From, UserMsg}
+%% UserMsg = {nick, "Nick"}
+%% {irc, channel, Ref, From, ChanMsg}
+%% ChanMsg = join | part | topic | {topic, Text} | {msg, Text}
